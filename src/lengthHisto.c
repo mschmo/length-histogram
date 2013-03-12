610cb15bfda19818543a35318d47f546458b4066
Matt Schmoyer <mlschmoyer@temple.edu>
2013-03-11 22:23:57 -0400

    check_vert() added

--- src/lengthHisto.c
+++ src/lengthHisto.c
@@ -1,8 +1,13 @@
-/* Reads text file and prints histogram of the
+/* ============================================
+ * Word Length Histogram v1.0 
+ * ============================================
+ *
+ * Reads text file and prints histogram of the
  * frequencies of the word lengths in the file.
  *
  * Written by: Matthew Schmoyer
  * Date: March 11, 2013
+ * ============================================
  */ 
 
 #include <stdio.h>
@@ -12,6 +17,10 @@
 
 /* Function to itialize count_a to all 0s */
 void init_array(int count_a[]);
+
+/* Prompts user asking if they want a vertical histogram.
+ * Returns 1 if yes, 0 if no. */
+int check_vert();
 
 /* Reads text file f and fills count_a with frequencies
  * of the word lengths in f */
@@ -31,29 +40,21 @@ void print_hist_ver(int count_a[]);
 int max_a(int count_a[]);
 
 int main(int argc, char *argv[]) {
-	
 	int count_a[LENGTH]; /* Stores frequencies */
-	char vert;
+	int vert;
 	init_array(count_a); /* Initialize count_a to all 0s */
-
-	// Check for passed file argument
+	
+	/* Check for passed file argument */
 	if (argc != 2) {
 		printf("Error: Missing argument.\n");
 		return(-1);
 	}
 	else {
-		printf("Vertical histogram? [Y/n] ");
-		vert = getchar();
-		while (toupper(vert) != 'Y' && toupper(vert) != 'N') {
-			printf("Invalid response.  Enter y or n: ");
-			getchar();
-			vert = getchar();
-		}
 		if (fill_hist(count_a, argv[1])) {
-			if (toupper(vert) != 'Y')
-				print_hist_hor(count_a);
-			else
+			if (check_vert())
 				print_hist_ver(count_a);
+			else
+				print_hist_hor(count_a);
 		}
 	}
 	
@@ -66,15 +67,27 @@ void init_array(int count_a[]) {
 		count_a[i] = 0;
 }
 
+int check_vert() {
+	char vert;
+	printf("Vertical histogram? [Y/n] ");
+		vert = getchar();
+		while (toupper(vert) != 'Y' && toupper(vert) != 'N') {
+			printf("Invalid response.  Enter y or n: ");
+			getchar();
+			vert = getchar();
+		}
+	if (toupper(vert) == 'Y')
+		return(1);
+	else
+		return(0);
+}
+
 int fill_hist(int count_a[], char f_path[]) {
-	
 	char ch;
 	int count = 0;
 	FILE *f_in = fopen(f_path, "r");
-	
 	if (f_in == 0)
 		printf("Error: No File.\n");
-
 	else {
 		while ((ch = fgetc(f_in)) != EOF) {
 			if (ch == ' ' || ch == '\n' || ch == '\t') {
@@ -88,13 +101,11 @@ int fill_hist(int count_a[], char f_path[]) {
 		fclose(f_in);
 		return(1);
 	}
-
 	// File did not work
 	return(0);
 }
 
 int pun_test(char ch) {
-	
 	switch (ch) {
 		case '\'':
 			return(1);
@@ -118,9 +129,7 @@ int pun_test(char ch) {
 }
 
 void print_hist_hor(int count_a[]) {
-
 	int i, j;
-
 	for (i=1; i < LENGTH; i++) {
 		printf("%2d", i);
 		printf(" ");
@@ -131,9 +140,7 @@ void print_hist_hor(int count_a[]) {
 }
 
 void print_hist_ver(int count_a[]) {
-
 	int i, j;
-
 	for (i = max_a(count_a); i > 0; i--) {
 		for (j = 1; j < LENGTH; j++) {
 			if (count_a[j] < i)
